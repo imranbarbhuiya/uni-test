17e6aaeca50f70a6222a1801a3ee807210b52468
parbez <imranbarbhuiya.fsd@gmail.com>
2026-01-16 16:13:05 +0530

    hello github

---
+++ metro.config.js
@@ -0,0 +1,12 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+const { withUniwindConfig } = require('uniwind/metro');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+module.exports = withUniwindConfig(config, {
+  cssEntryFile: './globals.css',
+  extraThemes: ['sepia', 'bubblegum'],
+  debug: true,
+});
